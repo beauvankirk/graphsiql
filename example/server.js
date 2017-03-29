48ac605dfafc2d7ab558291df253c1d072f60fe6
Beau Van Kirk <beauvankirk@gmail.com>
2017-03-29 11:22:30 -0500

    change html script refs to local for offline, use material theme

--- example/server.js
+++ example/server.js
@@ -5,6 +5,7 @@
  *  This source code is licensed under the license found in the
  *  LICENSE file in the root directory of this source tree.
  */
+ const path = require('path');
 
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
@@ -13,6 +14,10 @@ const schema = require('./schema');
 
 const app = express();
 app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname,'../')));
+app.use(express.static(path.join(__dirname,'./node_modules/codemirror/theme')));
+app.use(express.static(path.join(__dirname,'./node_modules/graphql')));
+
 app.use('/graphql', graphqlHTTP(() => ({ schema })));
 
 app.listen(0, function() {
